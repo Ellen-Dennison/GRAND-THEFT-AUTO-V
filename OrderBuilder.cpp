fe056ad3c14fdfdd8eebbc1db61ffa1fee16ab6a
Kwanele-P <133484538+Kwanele-P@users.noreply.github.com>
2025-10-21 08:10:46 +0200

    Create OrderBuilder.cpp

---
+++ OrderBuilder.cpp
@@ -0,0 +1,24 @@
+#include "OrderBuilder.h"
+
+OrderBuilder::OrderBuilder() {
+}
+
+OrderBuilder* OrderBuilder::setCustomer(string name) {
+
+}
+
+OrderBuilder* OrderBuilder::setPlant(Plant* plant) {
+
+}
+
+OrderBuilder* OrderBuilder::addDecorativePot() {
+
+}
+
+OrderBuilder* OrderBuilder::addGiftWrapping() {
+
+}
+
+PlantOrder* OrderBuilder::build() {
+
+}
