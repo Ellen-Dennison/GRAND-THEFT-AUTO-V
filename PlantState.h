43e44eae3812d5feee626f1f01086226d6b9293d
Kwanele-P <133484538+Kwanele-P@users.noreply.github.com>
2025-10-21 07:18:46 +0200

    Create PlantState.h

---
+++ PlantState.h
@@ -0,0 +1,19 @@
+#ifndef PLANTSTATE_H
+#define PLANTSTATE_H
+
+#include <iostream>
+#include <string>
+
+class PlantState{
+protected:
+    Plant* plant;
+
+public:
+    virtual ~PlantState(){};
+    void setPlant(Plant* p);
+    virtual void grow() = 0;
+    virtual std::string getStateName() const = 0;
+    virtual bool canBeSold() const = 0;
+};
+
+#endif
