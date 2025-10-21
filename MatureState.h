7cfe2387455233e3e07a08c6b1d461ed15ba6368
Kwanele-P <133484538+Kwanele-P@users.noreply.github.com>
2025-10-21 07:46:00 +0200

    Create MatureState.h

---
+++ MatureState.h
@@ -0,0 +1,16 @@
+#ifndef MATURESTATE_H
+#define MATURESTATE_H
+
+#include "PlantState.h"
+#include <iostream>
+#include <string>
+
+class MatureState : public PlantState{
+public:
+    void grow() override;
+    std::string getStateName() const override;
+    bool canBeSold() const override;
+
+};
+
+#endif
