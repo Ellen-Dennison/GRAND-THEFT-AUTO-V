fe52809a31cd762b318a204bb24d30de28d4df19
Kwanele-P <133484538+Kwanele-P@users.noreply.github.com>
2025-10-21 08:19:57 +0200

    Create PlantIterator.cpp

---
+++ PlantIterator.cpp
@@ -0,0 +1,16 @@
+#include "PlantIterator.h"
+#include "Iterator.h"
+
+PlantIterator::PlantIterator(vector<Plant*>& p) : plants(p), position(0) {}
+
+bool PlantIterator::hasNext() {
+
+}
+
+Plant* PlantIterator::next() {
+
+}
+
+void PlantIterator::reset() {
+
+}
