3f216add452a24786575b88edd671013852ef9b2
mbalz916 <makopimbalithobejane@gmail.com>
2025-10-27 23:40:24 +0200

    Implementation of WaterPlantsCommand.cpp

--- WaterPlantsCommand.cpp
+++ WaterPlantsCommand.cpp
@@ -1,5 +1,46 @@
-//
-// Created by user on 10/26/2025.
-//
-
 #include "WaterPlantsCommand.h"
+#include <iostream>
+
+WaterPlantsCommand::WaterPlantsCommand(std::vector<Plant*>& plantCollection) : plants(plantCollection) {
+
+}
+WaterPlantsCommand::~WaterPlantsCommand(){
+
+}
+
+void WaterPlantsCommand::execute() {
+    if(plants.empty()) {
+        std::cout << "No plants to water." << std::endl;
+        return;
+    }
+    std::cout << "\n------ Watering Plants ------" << std::endl;
+    std::cout << "Watering " << plants.size() << " plant (s)..." << std::endl;
+
+    int wateredCount = 0;
+
+    for(Plant* plant : plants) {
+        if(plant != nullptr) {
+            plant->water();
+            wateredCount++;
+
+        }else {
+            std::cerr << "Warning: Encountered null pointer, skipping..." << std::endl;
+
+        }
+    }
+    std::cout << "Successfully watered " << wateredCount << " plant(s)." << std::endl;
+    std::cout << "===========================\n" << std::endl;
+}
+
+std::string WaterPlantsCommand::getDescription() const {
+    return "Water " + std::to_string(plants.size()) +  " plant(s) in collection";
+}
+
+bool WaterPlantsCommand::isUndoable() const {
+    return false;
+}
+
+int WaterPlantsCommand::getPlantCount() const{
+    return static_cast<int>(plants.size());
+}
+
