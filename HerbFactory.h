ab0dc37fe3854e2f5fb77c3686890062e90a0e81
Kwanele-P <133484538+Kwanele-P@users.noreply.github.com>
2025-10-29 17:59:04 +0200

    Create HerbFactory.h

---
+++ HerbFactory.h
@@ -0,0 +1,20 @@
+#ifndef HERBFACTORY_H
+#define HERBFACTORY_H
+
+#include "PlantFactory.h"
+#include "Herb.h"
+
+class HerbFactory : public PlantFactory {
+private:
+    Plant* prototype;
+    std::string herbName;
+    double herbPrice;
+
+public:
+    HerbFactory(std::string name, double price);
+    ~HerbFactory();
+    Plant* createPlant() override;
+    std::string getFactoryType() const override;
+};
+
+#endif
