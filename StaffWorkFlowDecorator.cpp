4ca06e03978b5a12579b01e608b02d2b4c52510c
NomziPhosa <157846833+NomziPhosa@users.noreply.github.com>
2025-10-29 09:11:59 +0200

    Added Decorator class and the main file

---
+++ StaffWorkFlowDecorator.cpp
@@ -0,0 +1,31 @@
+#include "StaffWorkFlowDecorator.h"
+
+StaffWorkFlowDecorator::StaffWorkFlowDecorator(StaffWorkFlow* staff) {
+  wrappedStaff = staff;
+}
+
+void StaffWorkFlowDecorator::performMainTask() {
+  if (wrappedStaff)
+    wrappedStaff->performMainTask();
+}
+
+StaffWorkFlowDecorator::~StaffWorkFlowDecorator(){
+	delete wrappedStaff;
+}
+
+std::string StaffWorkFlowDecorator::getName(){
+    return wrappedStaff->getName();
+}
+
+void StaffWorkFlowDecorator::displayRoles(){
+  if(wrappedStaff){
+    wrappedStaff->displayRoles;
+  }
+}
+
+std::string StaffWorkFlowDecorator::getType(){
+  if(wrappedStaff){
+    return wrappedStaff->getType();
+  }
+  return "Unknown Type";
+}
