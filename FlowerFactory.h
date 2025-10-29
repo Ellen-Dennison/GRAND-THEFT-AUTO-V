8ebecc938ecc14acfe1bc37718e5e2a179857812
Kwanele-P <133484538+Kwanele-P@users.noreply.github.com>
2025-10-29 18:00:39 +0200

    Create FlowerFactory.h

---
+++ FlowerFactory.h
@@ -0,0 +1,20 @@
+#ifndef FLOWERFACTORY_H
+#define FLOWERFACTORY_H
+
+#include "PlantFactory.h"
+#include "Flower.h"
+
+class FlowerFactory : public PlantFactory {
+private:
+    Plant* prototype;
+    std::string flowerName;
+    double flowerPrice;
+
+public:
+    FlowerFactory(std::string name, double price);
+    ~FlowerFactory();
+    Plant* createPlant() override;
+    std::string getFactoryType() const override;
+};
+
+#endif
