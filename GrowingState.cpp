96f5aa946d31adb00576723c68f552ace9bb2c03
Kwanele-P <133484538+Kwanele-P@users.noreply.github.com>
2025-10-29 16:09:25 +0200

    Fixed segfault cause

--- GrowingState.cpp
+++ GrowingState.cpp
@@ -1,17 +1,22 @@
 #include "GrowingState.h"
 #include "MatureState.h"
+#include "Plant.h"
+
+GrowingState::GrowingState() : PlantState() {
+}
 
 void GrowingState::grow() {
-    if(plant->getAge() > 7 && plant->getHealth()>60){
+    if(plant->getAge() > 7 && plant->getHealth() > 60){
+        std::string plantName = plant->getName(); //get name first
         plant->setState(new MatureState());
-        std::cout<<plant->getName()<<" transitioned to Mature State."<<std::endl;
+        std::cout << plantName << " transitioned to Mature State." << std::endl;
     }
 }
 
-std::string GrowingState::getStateName(){
+std::string GrowingState::getStateName() const{
     return "Growing";
 }
 
-bool GrowingState::canBeSold(){
+bool GrowingState::canBeSold() const{
     return false;
 }
