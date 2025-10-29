44ac28c8495e81b69965d69952d1094582940838
Kwanele-P <133484538+Kwanele-P@users.noreply.github.com>
2025-10-29 17:26:54 +0200

    Update Tree.h

--- Tree.h
+++ Tree.h
@@ -1,16 +1,14 @@
 #ifndef TREE_H
 #define TREE_H
+
 #include "Plant.h"
+#include "WateringStrategy.h"
+#include "Enums.h"
 
-
-class Tree:public Plant
-{
-    public:
-    Tree(std::string name, std::string type, double price):Plant(name,type,price)
-    {};//!CHANGES
-    virtual ~Tree();//!CHANGES
-    Plant* clone() const;
-
+class Tree : public Plant {
+public:
+    Tree(std::string name, double price);
+    Plant* clone() const override;
 };
 
 #endif
