a1e4a524ad63d4e63d609ae5660fbc14f4a7a85b
Kwanele-P <133484538+Kwanele-P@users.noreply.github.com>
2025-10-29 16:13:39 +0200

    Fixed segFault cause
    Needed to get name first before deleting this*

--- SeedlingState.cpp
+++ SeedlingState.cpp
@@ -1,17 +1,22 @@
 #include "SeedlingState.h"
 #include "GrowingState.h"
+#include "Plant.h"
+
+SeedlingState::SeedlingState() : PlantState() {
+}
 
 void SeedlingState::grow() {
     if(plant->getAge() > 3){
+        std::string plantName = plant->getName();
         plant->setState(new GrowingState());
-        std::cout<<plant->getName()<<" transitioned to Growing State."<<std::endl;
+        std::cout << plantName << " transitioned to Growing State." << std::endl;
     }
 }
 
-std::string SeedlingState::getStateName(){
+std::string SeedlingState::getStateName() const{
     return "Seedling";
 }
 
-bool SeedlingState::canBeSold(){
+bool SeedlingState::canBeSold() const{
     return false;
 }
