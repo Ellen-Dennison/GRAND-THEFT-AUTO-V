13202dd995debb52caf8c1fa87b8002c4c45812f
Kwanele-P <133484538+Kwanele-P@users.noreply.github.com>
2025-11-01 08:45:45 +0200

    Updated with doxygen

--- src/GrowingState.h
+++ src/GrowingState.h
@@ -5,11 +5,35 @@
 #include <iostream>
 #include <string>
 
+/**
+ * @brief Represents the Growing state of a plant.
+ * The GrowingState class defines behavior for a fully grown plant.
+ * In this state, the plant cannot be sold and grows into the Mature state if it grows further.
+ */
 class GrowingState : public PlantState{
 public:
-    GrowingState() {};
+    /**
+     * @brief Constructs a new GrowingState object
+     */
+    GrowingState();
+
+    /**
+     * @brief Handles growth logic for the growing state
+     * Grows a plant further into the Mature state once it reaches age >7 and health >60
+     * Handles state transitions
+     */
     void grow();
-    std::string getStateName() const override;
+
+    /**
+     * @brief Returns the name of this state
+     * @return A string representing the state name - "Growing"
+     */
+    std::string getStateName()const override;
+
+    /**
+     * @brief Indicates whether the plant can be sold
+     * @return false as plant are not of age to be sold on this state
+     */
     bool canBeSold() const override;
 
 };
