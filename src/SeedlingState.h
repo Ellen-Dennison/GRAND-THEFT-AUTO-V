52cf9a24aee743bfe77a8d06138827b0aef3fc8f
Kwanele-P <133484538+Kwanele-P@users.noreply.github.com>
2025-11-01 08:52:28 +0200

    Updated with doxygen

--- src/SeedlingState.h
+++ src/SeedlingState.h
@@ -5,13 +5,36 @@
 #include <iostream>
 #include <string>
 
+/**
+ * @brief Represents the Seedling state of a plant
+ * The SeedlingState class defines behavior for a plant that has just been planted.
+ * In this state, the plant cannot be sold. It is in its early stages of growing
+ */
 class SeedlingState : public PlantState{
 public:
-    SeedlingState() {};
-    void grow();
+    /**
+     * @brief Constructs a new SeedlingState object
+     */
+    SeedlingState();
+
+    /**
+     * @brief Handles growth logic for the seedling state
+     * Plant grows and transitions into GrowingState
+     * This happens once it reaches an age > 3
+     */
+    void grow() override;
+
+    /**
+     * @brief Returns the name of this state
+     * @return A string representing the state name - "Seedling"
+     */       
     std::string getStateName() const override;
+
+    /**
+     * @brief Indicates whether the plant can be sold
+     * @return false. Plant is only a seed in this state
+     */
     bool canBeSold() const override;
-   
 
 };
 
