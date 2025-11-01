8383c8975d795fbf50c08a11ba4d67ef20af3036
Kwanele-P <133484538+Kwanele-P@users.noreply.github.com>
2025-11-01 08:45:17 +0200

    Updated with doxygen

--- src/FlowerFactory.h
+++ src/FlowerFactory.h
@@ -1,20 +1,40 @@
-#ifndef FLOWERFACTORY_H
-#define FLOWERFACTORY_H
+#ifndef FLOWERINGSTATE_H
+#define FLOWERINGSTATE_H
 
-#include "PlantFactory.h"
-#include "Flower.h"
+#include "PlantState.h"
+#include <iostream>
+#include <string>
 
-class FlowerFactory : public PlantFactory {
-private:
-    Plant* prototype;
-    std::string flowerName;
-    double flowerPrice;
-
+/**
+ * @brief Represents the Flowering state of a plant
+ * The FloweringState class defines behavior for a fully grown beautiful and wild plant.
+ * In this state, the plant can be sold and grows no further.
+ */
+class FloweringState : public PlantState{
 public:
-    FlowerFactory(std::string name, double price);
-    ~FlowerFactory();
-    Plant* createPlant() override;
-    std::string getFactoryType() const override;
+    /**
+     * @brief Constructs a new FloweringState object
+     */
+    FloweringState();
+
+    /**
+     * @brief Handles growth logic for the flowering state
+     * Plant does not grow any further
+     */
+    void grow();
+
+    /**
+     * @brief Returns the name of this state
+     * @return A string representing the state name - "Flowering"
+     */
+    std::string getStateName() const override;
+
+    /**
+     * @brief Indicates whether the plant can be sold
+     * @return true. Plants are of age and ready to attract customers
+     */
+    bool canBeSold() const override;
+
 };
 
 #endif
