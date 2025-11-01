9d58c690d1816cae55869d28434c4ca4eb239f8b
Kwanele-P <133484538+Kwanele-P@users.noreply.github.com>
2025-11-01 08:43:50 +0200

    Updated with doxygen

--- src/DailyWateringStrategy.h
+++ src/DailyWateringStrategy.h
@@ -8,9 +8,26 @@
 
 class Plant;
 
+/**
+ * @brief Concrete watering strategy that waters plants Daily
+ * 
+ * The DailyWateringStrategy class defines a watering behavior where
+ * plants receive a fixed amount of water daily, improving their health
+ * by a set number of points.
+ */
+
 class DailyWateringStrategy : public WateringStrategy{
     public:
+    /**
+     * @brief Water plants accordingly. Daily each at 500ml, adding 15 health points
+     * @param plant pointer to the plant object to be watered
+     */
     void water(Plant* plant);
+
+    /**
+     * @brief Retrieves watering schedule for plant type
+     * @return watering schedule in string form
+    */   
     std::string getWateringSchedule() const;
 };
 
