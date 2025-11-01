fe89e2830876c165c7f90c89eaef702f81aa276a
Kwanele-P <133484538+Kwanele-P@users.noreply.github.com>
2025-11-01 08:53:42 +0200

    Updated with doxygen

--- src/WiltingState.h
+++ src/WiltingState.h
@@ -5,11 +5,36 @@
 #include <iostream>
 #include <string>
 
+/**
+ * @brief Represents the Wilting state of a plant
+ * The WiltingState class defines behavior for a plant that has been neglected to the point of no return.
+ * In this state, the plant can neither be sold nor revived
+ */
 class WiltingState : public PlantState{
 public:
-    WiltingState() {};
-    void grow();
+    /**
+     * @brief Constructs a new WiltingState object
+     */
+    WiltingState();
+
+    /**
+     * @brief Handles growth logic for the wilting state
+     * Plant does not grow any further although it can be revived as long as the plants's health remains >0
+     * A plant enters this state once it reaches a health level <40
+     * Plant starts declining at 5 health levels as it s wilting
+     */
+    void grow() override;
+
+    /**
+     * @brief Returns the name of this state
+     * @return A string representing the state name - "Wilting"
+     */   
     std::string getStateName() const override;
+
+    /**
+     * @brief Indicates whether the plant can be sold
+     * @return false. Plant is dying
+     */
     bool canBeSold() const override;
 
 };
