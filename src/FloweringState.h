393cab6ce0ffefae14fc4318a4cd8eb1a2cdbdc5
Kwanele-P <133484538+Kwanele-P@users.noreply.github.com>
2025-11-02 03:17:40 +0200

    Update FloweringState.h

--- src/FloweringState.h
+++ src/FloweringState.h
@@ -5,12 +5,34 @@
 #include <iostream>
 #include <string>
 
-class FloweringState : public PlantState
-{
+/**
+ * @brief Represents the Flowering state of a plant
+ * The FloweringState class defines behavior for a fully grown beautiful and wild plant.
+ * In this state, the plant can be sold and grows no further.
+ */
+class FloweringState : public PlantState{
 public:
-    FloweringState() {};
+    /**
+     * @brief Constructs a new FloweringState object
+     */
+    FloweringState();
+
+    /**
+     * @brief Handles growth logic for the flowering state
+     * Plant does not grow any further
+     */
     void grow();
+
+    /**
+     * @brief Returns the name of this state
+     * @return A string representing the state name - "Flowering"
+     */
     std::string getStateName() const override;
+
+    /**
+     * @brief Indicates whether the plant can be sold
+     * @return true. Plants are of age and ready to attract customers
+     */
     bool canBeSold() const override;
 
 };
