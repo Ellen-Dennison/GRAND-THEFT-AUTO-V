dd36fb7bb17c3341845ce0dcd211a1a6aeb53fdd
Kwanele-P <133484538+Kwanele-P@users.noreply.github.com>
2025-11-02 12:45:11 +0200

    INHERITANCE

--- src/GrowingState.cpp
+++ src/GrowingState.cpp
@@ -2,12 +2,12 @@
 #include "MatureState.h"
 #include "Plant.h"
 
-GrowingState::GrowingState()
-{}
+GrowingState::GrowingState() : PlantState() {
+}
 
 void GrowingState::grow() {
     if(plant->getAge() > 7 && plant->getHealth() > 60){
-        std::string plantName = plant->getName(); //get name first
+        std::string plantName = plant->getName();
         plant->setState(new MatureState());
         std::cout << plantName << " transitioned to Mature State." << std::endl;
     }
