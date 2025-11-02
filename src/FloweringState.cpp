41b068bf7f11727a31f111792fa51e1e09d8a8d1
Kwanele-P <133484538+Kwanele-P@users.noreply.github.com>
2025-11-02 12:46:36 +0200

    INHERITANCE

--- src/FloweringState.cpp
+++ src/FloweringState.cpp
@@ -1,19 +1,16 @@
 #include "FloweringState.h"
 
-FloweringState::FloweringState()
-{}
+FloweringState::FloweringState() : PlantState() {
+}
 
-void FloweringState::grow() 
-{
+void FloweringState::grow() {
     //can't grow further. stays
 }
 
-std::string FloweringState::getStateName() const
-{
+std::string FloweringState::getStateName() const{
     return "Flowering";
 }
 
-bool FloweringState::canBeSold() const
-{
+bool FloweringState::canBeSold() const{
     return true;
 }
