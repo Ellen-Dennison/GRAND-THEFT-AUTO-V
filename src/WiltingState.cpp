36b0ae5ded56d06579fdf50a8c2760a366165101
Kwanele-P <133484538+Kwanele-P@users.noreply.github.com>
2025-11-02 12:42:05 +0200

    INHERITANCE

--- src/WiltingState.cpp
+++ src/WiltingState.cpp
@@ -1,31 +1,25 @@
 #include "WiltingState.h"
 #include "DeadState.h"
+#include "Plant.h"
 
+WiltingState::WiltingState() : PlantState() {
+}
 
-WiltingState::WiltingState()
-{}
-
-
-
-void WiltingState::grow() 
-{
+void WiltingState::grow() {
     plant->adjustHealth(-5);
     std::cout<<plant->getName()<<" is wilting. Health declining..."<<std::endl;
 
-    if(plant->getHealth() <=0)
-    {
-        std::cout<<plant->getName()<<" has DIED."<<std::endl;
-        plant->setState(new DeadState);
-        
+    if(plant->getHealth() <=0){
+        std::string plantName = plant->getName();
+        plant->setState(new DeadState());
+        std::cout<<plantName<<" has DIED."<<std::endl;
     }
 }
 
-std::string WiltingState::getStateName() const
-{
+std::string WiltingState::getStateName() const{
     return "Wilting";
 }
 
-bool WiltingState::canBeSold() const
-{
+bool WiltingState::canBeSold() const{
     return false;
 }
