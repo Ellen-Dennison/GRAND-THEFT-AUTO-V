bd32f2486b8b3746a2acce97274180eed20cdddb
NomziPhosa <157846833+NomziPhosa@users.noreply.github.com>
2025-11-04 01:43:05 +0200

    implemented the Explore button

--- gui/Unit1.cpp
+++ gui/Unit1.cpp
@@ -4,6 +4,7 @@
 #pragma hdrstop
 
 #include "Unit1.h"
+#include "Unit2.h"
 //---------------------------------------------------------------------------
 #pragma package(smart_init)
 #pragma resource "*.fmx"
@@ -12,5 +13,11 @@ TForm1 *Form1;
 __fastcall TForm1::TForm1(TComponent* Owner)
 	: TForm(Owner)
 {
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm1::ExploreClick(TObject *Sender)
+{
+	Form2->Show();
+	this->Hide();
 }
 //---------------------------------------------------------------------------
